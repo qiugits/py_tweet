3796b046e39e9c88ebf4a08d2bf014eb95549bcb
Qiu <qiu.gits@gmail.com>
2018-02-02 16:38:01 +0900

    Easy cli setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='twpy',
     version='1.0',
+    packages=find_packages(),
     install_requires=[],
     entry_points={
         'console_scripts': 'twpy = twpy.__main__:main'
@@ -13,6 +14,10 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
